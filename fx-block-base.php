851c56039dee88e74f8df0ad5200216220da972f
turtlepod <david@shellcreeper.com>
2022-03-21 02:31:14 +0700

    remove plugin constants

--- fx-block-base.php
+++ fx-block-base.php
@@ -10,12 +10,5 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 **/
 
-// Plugin Constants.
-define( 'FXBB_VERSION', '1.0.1' );
-define( 'FXBB_PATH', plugin_dir_path(__FILE__) );
-define( 'FXBB_URI', plugin_dir_url( __FILE__ ) );
-define( 'FXBB_INC', FXBB_PATH . 'includes/' );
-define( 'FXBB_BLOCK_DIR', FXBB_PATH . 'blocks/' );
-
 // Load blocks.
-require_once FXBB_BLOCK_DIR . 'blocks.php';
+require_once plugin_dir_path(__FILE__) . 'blocks/blocks.php';
