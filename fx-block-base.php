b5ca8482ef8083354a23445c55a7a0390921790a
turtlepod <david@shellcreeper.com>
2022-03-21 02:53:06 +0700

    update plugin url.

--- fx-block-base.php
+++ fx-block-base.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: f(x) Block Base
- * Plugin URI: http://genbumedia.com/plugins/fx-updater/
+ * Plugin URI: https://github.com/turtlepod/fx-block-base
  * Description: Block starter plugin with examples.
  * Version: 1.0.0
  * Author: David Chandra Purnama
- * Author URI: http://turtlepod.xyz/
+ * Author URI: https://turtlepod.xyz/
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 **/
